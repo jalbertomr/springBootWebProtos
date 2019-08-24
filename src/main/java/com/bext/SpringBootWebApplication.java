149d2d296cd559727eeb71a6c63a8e52df0a14cd
bext <jalbertomr@yahoo.com>
2019-08-23 20:19:39 -0500

    se Sustituye instanciacion manual New con Estereotipos @Repository, @Service

--- src/main/java/com/bext/SpringBootWebApplication.java
+++ src/main/java/com/bext/SpringBootWebApplication.java
@@ -1,16 +1,22 @@
-package com.bext.springBootWeb;
+package com.bext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bext.service.IPersonaService;
+import com.bext.service.PersonaServiceImpl;
+
 @SpringBootApplication
 public class SpringBootWebApplication implements CommandLineRunner {
-	
 	private static Logger LOG = LoggerFactory.getLogger(SpringBootWebApplication.class);
 
+	@Autowired
+	private IPersonaService personaService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
@@ -18,7 +24,8 @@ public class SpringBootWebApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		LOG.info("Logging en SpringBoot.");
-		
+
+		personaService.alta("Juan Perez");
 	}
 
 }
